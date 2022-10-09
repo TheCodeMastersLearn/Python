eb0cc782892ee4b80395c44c6753f31c010333e4
Juan C Carpinteyro Jr <juanccarpinteyrojr@Juans-MacBook-Air.local>
2022-10-09 13:54:06 -0600

    first project done

--- calculateCirclearea.py
+++ calculateCirclearea.py
@@ -3,6 +3,13 @@
 
 pi = 3.14
 result = 0
+radius = -1
+
+radius = input("Enter radius number")
+
+result = (pi * int(radius) ** 2)
+
+print (result)
 
 # get the user input using input() :::::: reference -> https://www.pythonforbeginners.com/basics/how-to-take-user-input-in-python#:~:text=We%20can%20use%20the%20input%28%29%20method%20to%20take,value%20entered%20by%20the%20user%20as%20a%20string.
 
