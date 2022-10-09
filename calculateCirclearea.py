132dc0a6e81d25deaf21be402181a08cc525d86a
Juan C Carpinteyro Jr <juanccarpinteyrojr@Juans-MacBook-Air.local>
2022-10-09 14:00:01 -0600

    This is the actual first project done to james standards

--- calculateCirclearea.py
+++ calculateCirclearea.py
@@ -5,17 +5,14 @@ pi = 3.14
 result = 0
 radius = -1
 
-radius = input("Enter radius number")
-
-result = (pi * int(radius) ** 2)
-
-print (result)
-
 # get the user input using input() :::::: reference -> https://www.pythonforbeginners.com/basics/how-to-take-user-input-in-python#:~:text=We%20can%20use%20the%20input%28%29%20method%20to%20take,value%20entered%20by%20the%20user%20as%20a%20string.
 
-
+radius = input("Enter radius number")
 
 # do calculations and set the result variable to it
 
+result = (pi * int(radius) ** 2)
 
 # print results back to the user
+
+print (result)
