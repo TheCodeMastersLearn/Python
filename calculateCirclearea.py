c3612058a37962c2291278973fea70ef501a91af
james <slumburger15@gmail.com>
2022-10-09 13:31:09 -0600

    first program

---
+++ calculateCirclearea.py
@@ -0,0 +1,14 @@
+# You need to use pi (3.14) r^2 to calcualte a cirlce area 
+
+
+pi = 3.14
+result = 0
+
+# get the user input using input() :::::: reference -> https://www.pythonforbeginners.com/basics/how-to-take-user-input-in-python#:~:text=We%20can%20use%20the%20input%28%29%20method%20to%20take,value%20entered%20by%20the%20user%20as%20a%20string.
+
+
+
+# do calculations and set the result variable to it
+
+
+# print results back to the user
