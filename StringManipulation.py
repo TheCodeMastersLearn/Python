b93e7a793b1ff950e6b8e15fcf4f1b1283d4286c
james <slumburger15@gmail.com>
2022-10-09 17:28:32 -0600

    string manipulation

---
+++ StringManipulation.py
@@ -0,0 +1,40 @@
+# in this one you will practice string manipulation. 
+# string manipulation is when you manipulate a string :D 
+# this could be to combine strings into one string : "james" and " marshall" into "james marshall"
+    # this is called concatination. You can use the + opperator to combine strings. 
+    # fullname = "james" + " marshall"
+    # 
+
+# it could also replace part of the string with other strings: "james is the cool" -> "james is the coolest"
+    # this is called replacing you can use the variable.replace() function to replace parts of a string
+    # fullname = "james is the cool"
+    # replaced = fullname.replace("cool", "coolest")
+
+# it can also be used to strip away parts of the string incase you only want certain information 
+    # "james, marshall, 24, male" -> "james marshall 24 male"
+    # this is called stripping, you can use the variable.strip() function for this.
+    # fullname = "james, marshall, 24, male"
+    # strippedNames = fullname.strip(",")
+
+# you can also split strings -> "james philip" -> "james", "philip"
+    # this is called splitting, you can use the variable.split() function to split strings. 
+    # fullname =  "james philip" 
+    # splitnames = fullname.split(" ")
+
+
+# now ask the user for their full name and their age.
+
+
+# ask them for a comma seperated list of three of their family members firstnames and their ages (like this : philip 22, james 24, sarah 100)
+
+
+# print out "welcome " + username + "."
+
+# print out the family information split by the ","
+
+# print out the family information split by the " "
+
+# tell the user what their full name would be if they changed their last name to "salmon"
+
+# strip away all the "," in the family informaiton and print it out.
+
