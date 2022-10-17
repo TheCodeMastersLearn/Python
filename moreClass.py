e4427535b63e7f54fac026cec27d010ea4520a15
Juan C Carpinteyro Jr <juanccarpinteyrojr@Juans-MacBook-Air.local>
2022-10-16 18:00:24 -0600

    Project Complete

--- moreClass.py
+++ moreClass.py
@@ -47,10 +47,307 @@
 
 # so create a person class that has any attributes or methods you think it should have. 
 
+from cgi import print_arguments
 
 
+class person:
+    
+    #######################
+    # Constructor: __init__
+    #
+    # Parameters: 
+    # gender: string, height: float, hair_color: string, weight: float, eye_color: string, age: int)
+    #
+    # comments
+    # This is the constructor.
+    ######################
+    def __init__(self, gender, height, hair_color, weight, eye_color, age):
+        self.gender = gender
+        self.height = height
+        self.hair_color = hair_color
+        self.weight = weight
+        self.eye_color = eye_color
+        self.age = age
 
+    #######################
+    # Method Name: get_gender
+    # 
+    # Return Type: String
+    #
+    # comments
+    # This will get gender from the class.
+    ######################
+    def get_gender(self):
+        return self.gender
 
+    #######################
+    # Method Name: get_height
+    # 
+    # Return Type: String
+    #
+    # comments
+    # This will get height from the class.
+    ######################
+    def get_height(self):
+        return self.height
+
+    #######################
+    # Method Name: get_hair_color
+    # 
+    # Return Type: String
+    #
+    # comments
+    # This will get hair_color from the class.
+    ######################
+    def get_hair_color(self):
+        return self.hair_color
+
+    #######################
+    # Method Name: get_weight
+    # 
+    # Return Type: float
+    #
+    # comments
+    # This will get weight from the class.
+    ######################
+    def get_weight(self):
+        return self.weight
+
+    #######################
+    # Method Name: get_eye_color
+    # 
+    # Return Type: String
+    #
+    # comments
+    # This will get eye_color from the class.
+    ######################
+    def get_eye_color(self):
+        return self.eye_color
+
+    #######################
+    # Method Name: get_age
+    # 
+    # Return Type: int
+    #
+    # comments
+    # This will get age from the class.
+    ######################
+    def get_age(self):
+        return self.age
+
+    #######################
+    # Method Name: set_gender
+    # 
+    # Return Type: String
+    #
+    # comments
+    # This will set gender in the class.
+    ######################
+    def set_gender(self, gender):
+        self.gender = gender
+
+    #######################
+    # Method Name: set_height
+    # 
+    # Return Type: String
+    #
+    # comments
+    # This will set height in the class.
+    ######################
+    def set_height(self, height):
+        self.height = height
+
+    #######################
+    # Method Name: set_hair_color
+    # 
+    # Return Type: String
+    #
+    # comments
+    # This will set hair_color in the class.
+    ######################
+    def set_hair_color(self, hair_color):
+        self.hair_color = hair_color
+
+    #######################
+    # Method Name: set_weight
+    # 
+    # Return Type: float
+    #
+    # comments
+    # This will set weight in the class.
+    ######################
+    def set_weight(self, weight):
+        self.weight = weight
+
+    #######################
+    # Method Name: set_eye_color
+    # 
+    # Return Type: String
+    #
+    # comments
+    # This will set eye_color in the class.
+    ######################
+    def set_eye_color(self, eye_color):
+        self.eye_color = eye_color
+
+    #######################
+    # Method Name: set_age
+    # 
+    # Return Type: int
+    #
+    # comments
+    # This will set age in the class.
+    ######################
+    def set_age(self, age):
+        self.age = age
+
+    #######################
+    # Method Name: make_sound
+    # 
+    # Return Type: String
+    #
+    # comments
+    # This will print "Hi." to the terminal.
+    ######################
+    def make_sound():
+        print ("Hi.")
+
+class businessman(person):
+  
+    #######################
+    # Constructor: __init__
+    #
+    # Parameters: 
+    # gender: string, height: float, hair_color: string, weight: float, eye_color: string, age: int
+    # educatiion: string, last_job: string, salary: float, years_of_exp: int
+    #
+    # comments
+    # This is the constructor.
+    ######################
+    def __init__(self, gender, height, hair_color, weight, eye_color, age, educatiion, last_job, salary, years_of_exp):
+        super().__init__(gender, height, hair_color, weight, eye_color, age)
+        self.education = educatiion
+        self.last_job = last_job
+        self.salary = salary
+        self.years_of_exp = years_of_exp
+
+    #######################
+    # Method Name: get_education
+    # 
+    # Return Type: String
+    #
+    # comments
+    # This will get education from the class.
+    ######################
+    def get_education(self):
+        return self.education
+
+    #######################
+    # Method Name: get_last_job
+    # 
+    # Return Type: String
+    #
+    # comments
+    # This will get last_job from the class.
+    ######################
+    def get_last_job(self):
+        return self.last_job
+
+    #######################
+    # Method Name: get_salary
+    # 
+    # Return Type: float
+    #
+    # comments
+    # This will get salary from the class.
+    ######################
+    def get_salary(self):
+        return self.salary
+
+    #######################
+    # Method Name: get_years_of_exp
+    # 
+    # Return Type: int
+    #
+    # comments
+    # This will get years_of_exp from the class.
+    ######################
+    def get_years_of_exp(self):
+        return self.years_of_exp
+
+    #######################
+    # Method Name: set_education
+    # 
+    # Return Type: String
+    #
+    # comments
+    # This will set education in the class.
+    ######################
+    def set_education(self, education):
+        self.education = education
+
+    #######################
+    # Method Name: set_last_job
+    # 
+    # Return Type: String
+    #
+    # comments
+    # This will set last_job in the class.
+    ######################
+    def set_last_job(self, last_job):
+        self.last_job = last_job
+
+    #######################
+    # Method Name: set_salary
+    # 
+    # Return Type: float
+    #
+    # comments
+    # This will set salary in the class.
+    ######################
+    def set_salary(self, salary):
+        self.salary = salary
+
+    #######################
+    # Method Name: set_years_of_exp
+    # 
+    # Return Type: int
+    #
+    # comments
+    # This will set years_of_exp in the class.
+    ######################
+    def set_years_of_exp(self, years_of_exp):
+        self.years_of_exp = years_of_exp
+
+    #######################
+    # Method Name: professional_speak
+    # 
+    # Return Type: String
+    #
+    # comments
+    # This will print "Greetings" to the terminal.
+    ######################
+    def professional_speak():
+        print ("Greetings")
+
+philip = businessman("Male", str("5'8\" "), "brown", 210, "brown", 22, "Some college", "Jimmy Johns", 25000, 2)
+sarah = person("Female", str("5'4\" "), "brown", 130, "brown", 27)
+james = businessman("male", str("5'10\" "), "brown", 200, "brown", 24, "Some college", "Cresco", 56160, str(">1"))
+
+if james.get_weight() > sarah.get_weight():
+    print ("James is fatter than Sarah")
+else:
+    print ("You messed up.")
+
+if james.get_weight() > philip.get_weight():
+    print ("James you need to hit the gym.")
+else:
+    print ("Philip you need to hit the gym.")
+
+if james.get_age() > 20 and philip.get_age() > 20:
+    print ("You guys are getting so old.")
+else:
+    print ("You messed up.")    
 
 # and now create a buisnessman class that inherits from the person class. Give him some things that is specific to buisnessmen:
 # like a suit attribute that you can set the color of. 
@@ -58,7 +355,7 @@
 # etc
 
 # you inherit from class in python by in the class definition adding parenthesees with the parent class in it. 
-# EX: class buisnessman(Person):
+# EX: class businessman(Person):
 #           ......
 
 
